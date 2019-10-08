ccd609383a2801efc49c274b931824ce1db30fad
KirillFilipp <kirill_filipp@mail.ru>
2019-10-09 00:25:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+    let weeks, hours = 1300;
+    if (knowsProgramming === true) {
+        hours = 800;
+    }
+    weeks = hours / config[focus];
+    return Math.ceil(weeks);
+};
